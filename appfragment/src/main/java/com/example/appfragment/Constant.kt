bf2a69d147ce6fce95e3012849786710375ff082
trietpham154 <trietpham154@gmail.com>
2020-05-11 17:23:01 +0700

    create Constant class

---
+++ appfragment/src/main/java/com/example/appfragment/Constant.kt
@@ -0,0 +1,9 @@
+package com.example.appfragment
+
+final class Constant {
+    companion object {
+        const val LIST_DATA_KEY = "mListData"
+        const val CONTENT_KEY = "content"
+        const val CANT_GET_CONTENT = "Can't get content"
+    }
+}
